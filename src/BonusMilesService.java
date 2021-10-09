3d6cf717ffe82b830081b9d08760b51ce310e74e
DMITRY KRAVCHENKO <88271958+DmitryK8@users.noreply.github.com>
2021-10-09 22:56:39 +0500

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService<bonusMilesService> {
 
-    public int calculate(int price, int cost) {
+    public int calculate(int price) {
         int bonusMilesService = price / cost;
 
         return bonusMilesService;
